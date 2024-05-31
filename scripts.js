716d6a36d476740141d121dd3d67112fc86c2ff0
AngelStacks <polarg7531@gmail.com>
2024-05-31 12:09:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,6 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 window.location.href = 'Parte3.html';
             } else if (window.location.pathname.includes('Parte3.html')) {
                 window.location.href = 'Parte4.html';
+            } else if (window.location.pathname.includes('Parte5.html')) {
+                window.location.href = 'Parte6.html';
             } 
         });
     };
