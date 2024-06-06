ceea59e12d991e4039792a3c206c2d7ef7c1d12b
AngelStacks <polarg7531@gmail.com>
2024-06-06 12:04:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -73,11 +73,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.getElementById('menuButton');
-    const menu = document.getElementById('menu');
-
-    menuButton.addEventListener('click', function() {
-        menu.classList.toggle('activo');
-    });
-});
